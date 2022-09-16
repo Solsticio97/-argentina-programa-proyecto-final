9ebd6ff17a32321dc2e17791d6610c6ad839c32a
Solsticio97 <arnal.programacion@gmail.com>
2022-09-16 13:59:18 -0300

    commit de angular y modulos

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { FormacionAcademicaComponent } from './components/formacion-academica/formacion-academica.component';
+import { HardSoftComponent } from './components/hard-soft/hard-soft.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    FormacionAcademicaComponent,
+    HardSoftComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
